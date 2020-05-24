6f349e2a3d7ecff9f989c161d6dd4441ddb6de85
Gregory Vincic <g@7de.se>
2020-05-24 09:45:12 +0200

    Language concepts

---
+++ lang/concept.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// arrays and slices: types and ranges
+	// maps: Printf vs. loop
+	// structs + methods: Device String
+	// embedding: Vehicles and cars
+	// interfaces: Printer
+	// go routines and channels: doSomething, sync
+}
